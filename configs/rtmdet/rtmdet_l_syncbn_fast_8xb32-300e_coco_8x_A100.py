d4e0e8d49e9469925b93d405dcd0f9b64c74e6b7
charlescao460 <charlescao@csr460.com>
2025-01-20 19:37:39 -0500

    Update

--- configs/rtmdet/rtmdet_l_syncbn_fast_8xb32-300e_coco_8x_A100.py
+++ configs/rtmdet/rtmdet_l_syncbn_fast_8xb32-300e_coco_8x_A100.py
@@ -62,9 +62,9 @@ batch_shapes_cfg = dict(
 
 # -----model related-----
 # The scaling factor that controls the depth of the network structure
-deepen_factor = 0.67
+deepen_factor = 1.0
 # The scaling factor that controls the width of the network structure
-widen_factor = 0.75
+widen_factor = 1.0
 # Strides of multi-scale prior box
 strides = [8, 16, 32]
 
